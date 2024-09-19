a1585f2bb4470d96c4f9e0d5de3d761aebabeb97
LuisVieira0701 <luisvieira@usf.edu>
2024-09-19 15:03:19 -0400

    Task1 Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,8 @@
+//Task 1 Create an inventory Array of Product Objects
+const inventory = [
+    {name: 'watch', price: 30, qunatity: 45},
+    {name: 'necklace', price: 40, quantity: 50},
+    {name: 'ring', price: 25, qunatity: 70},
+    {name: 'bracelet', price: 35, quantity: 55}
+]
+
