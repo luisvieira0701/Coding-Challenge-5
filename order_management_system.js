6bd31b4ed47727583620fe963dc0b51fa51d69e7
LuisVieira0701 <luisvieira@usf.edu>
2024-09-21 17:23:02 -0400

    Task3 Create PlaceOrder Function

--- order_management_system.js
+++ order_management_system.js
@@ -1,10 +1,41 @@
 //Task 1 Create an inventory Array of Product Objects
 const inventory = [
-    {name: 'watch', price: 30, qunatity: 45},
+    {name: 'watch', price: 30, quantity: 45},
     {name: 'necklace', price: 40, quantity: 50},
-    {name: 'ring', price: 25, qunatity: 70},
+    {name: 'ring', price: 25, quantity: 70},
     {name: 'bracelet', price: 35, quantity: 55}
 ]
 
 //Task 2 Create an Orders array of Order Objects
-const orders= [] //This array will contain customernames, items (name, and quantity), status
+let orders= [] //This array will contain customernames, items (name, and quantity), status
+
+
+
+//Task 3 Create a Function to place an Order
+function placeOrder(customerName, itemsOrdered, OrderStatus)  {
+    itemsOrdered.forEach(item => {
+        if (itemsOrdered.quantity < inventory.quantity) {
+                return console.log('ERROR: Item has insufficient stock for order')
+        } else {
+            let product = inventory.find((product => product.name = item.name))
+                return product.quantity -= item.Quantity
+        }})
+     orders.push({
+        customer:customerName,
+        jewelryProducts: itemsOrdered,
+        OrderStatus: status
+        
+        });
+        console.log (`${customerName} orders:`, itemsOrdered, OrderStatus )
+}
+
+// Testing the Function with orders
+placeOrder('Michael Myers', [
+    {name: 'watch', quantity: 15},
+    {name: 'necklace', quantity: 30},
+], OrderStatus = 'pending') 
+
+placeOrder('Freddy Krueger', [
+    {name: 'watch', quantity: 30},
+    {name: 'ring', quantity: 30}
+], OrderStatus = 'pending')
